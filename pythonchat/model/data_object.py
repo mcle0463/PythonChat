3908fcee43504501ba14739aacbf0a68bd3cde2b
G M <gremcleo@gmail.com>
2019-03-29 15:16:18 -0400

    change file structure to reflect mvc

---
+++ pythonchat/model/data_object.py
@@ -0,0 +1,97 @@
+# data_object
+# Greg McLeod
+# !/usr/bin/env python
+
+"""Class that will instantiate a data object and
+operate on data passed via the UI and csv files.
+"""
+__author__ = "Greg McLeod"
+__version__ = "1.1"
+__maintainer__ = "Greg McLeod"
+__email__ = "mcle0463@algonquincollge.com"
+__status__ = "Dev"
+
+# module imports
+import sys
+import csv
+import os
+
+
+class DataObject(object):
+    """A data object that can contain and manipulate csv file records
+    """
+
+    def __init__(self):
+        """Creating a data object"""
+        self.input_file_path = os.path.abspath(
+            "PythonChat\pythonchat\\functional\Quttinirpaaq_NP_Tundra_Plant_Phenology_2016-2017_data_1.csv")
+        self.records = list()
+        self.header = ""
+        self.record_selected = 0
+
+    def get_rows(self):
+        """Reads file and parses data to a list object"""
+        self.records.clear()
+        try:
+            input_file = open(self.input_file_path, 'rt')
+            reader = csv.reader(input_file)
+            for i, row in enumerate(reader):
+                if i >= 12:
+                    break
+                if i == 0:
+                    self.data_header = row
+                if i >= 2:
+                    self.records.append(list(row))
+            input_file.close()
+        except OSError as e:
+            print("I/O error({0}): {1}".format(e.errno, e.strerror))
+            print(self.input_file_path)
+        except Exception:
+            print("Unexpected error:", sys.exc_info()[0])
+
+    def print_data(self):
+        """Prints all records"""
+        data_string = "Greg McLeod" + "\n\n"
+        for record in self.records:
+            data_string += str(record)
+            data_string += "\n"
+        return data_string
+
+    def create_record(self, record_data):
+        """Creates list record"""
+        if self.record_selected != -1:
+            self.records.insert(self.record_selected, record_data)
+        else:
+            print("please select a record")
+
+    def select_record(self, selection):
+        """Selects list record"""
+        print("select record")
+        if selection >= 1 and selection <= len(self.records):
+                # break
+            self.record_selected = selection
+            print("Selected " + str(selection))
+
+    def display_record(self):
+        """displays currently selected list record"""
+        if self.record_selected != -1:
+            return str(self.records[self.record_selected])
+        else:
+            print("please select a record")
+
+    def edit_record(self, record_data):
+        """Edits currently selected list record"""
+        if self.record_selected != -1:
+            self.records[self.record_selected] = record_data
+        else:
+            print("please select a record")
+
+    def delete_record(self):
+        """Deletes currently selected list record"""
+        if self.record_selected != -1:
+            self.records.pop(self.record_selected)
+            print(self.record_selected)
+            print("has been deleted\n")
+            self.record_selected = -1
+        else:
+            print("please select a record")
---
+++ pythonchat/view/user_interface.py
@@ -0,0 +1,189 @@
+# user_interface
+# Greg McLeod
+# !/usr/bin/env python
+"""View
+"""
+__author__ = "Greg McLeod"
+__version__ = "1.1"
+__maintainer__ = "Greg McLeod"
+__email__ = "mcle0463@algonquincollge.com"
+__status__ = "Dev"
+
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class UserInterface(QtWidgets.QMainWindow):
+
+    def __init__(self, controller):
+        super().__init__()
+        print(__name__)
+        self.control = controller
+        self.initUI()
+
+    def initUI(self):
+        # set main widget and main widget layout
+        main_widget = QtWidgets.QWidget(self)
+        self.setCentralWidget(main_widget)
+        main_layout = QtWidgets.QVBoxLayout()
+        main_widget.setLayout(main_layout)
+        # set child layout objects
+        self.button_layout = QtWidgets.QHBoxLayout()
+        self.widget_layout = QtWidgets.QVBoxLayout()
+        # Build Qpushuttons
+        menu_option_titles = ["Reload data set", "Display full data set", "Create new data record",
+                              "Select a record", "Display a record", "Edit a record", "Delete a record", "Exit"]
+        # reload button
+        button = QtWidgets.QToolButton()
+        button.setText(menu_option_titles[0])
+        button.setObjectName('reload_button_clicked')
+        button.clicked.connect(self.reload_button_clicked)
+        button.setSizePolicy(
+            QtWidgets.QSizePolicy.Preferred,
+            QtWidgets.QSizePolicy.Preferred)
+        self.button_layout.addWidget(button)
+        # display full data button
+        button = QtWidgets.QToolButton()
+        button.setText(menu_option_titles[1])
+        button.setObjectName('display_full_data_button_clicked')
+        button.clicked.connect(self.display_full_data_button_clicked)
+        button.setSizePolicy(
+            QtWidgets.QSizePolicy.Preferred,
+            QtWidgets.QSizePolicy.Preferred)
+        self.button_layout.addWidget(button)
+        # Create New record button
+        button = QtWidgets.QToolButton()
+        button.setText(menu_option_titles[2])
+        button.setObjectName('create_new_record_button_clicked')
+        button.clicked.connect(self.create_new_record_button_clicked)
+        button.setSizePolicy(
+            QtWidgets.QSizePolicy.Preferred,
+            QtWidgets.QSizePolicy.Preferred)
+        self.button_layout.addWidget(button)
+        # Select record button
+        button = QtWidgets.QToolButton()
+        button.setText(menu_option_titles[3])
+        button.setObjectName('select_record_button_clicked')
+        button.clicked.connect(self.select_record_button_clicked)
+        button.setSizePolicy(
+            QtWidgets.QSizePolicy.Preferred,
+            QtWidgets.QSizePolicy.Preferred)
+        self.button_layout.addWidget(button)
+        # Display single record button
+        button = QtWidgets.QToolButton()
+        button.setText(menu_option_titles[4])
+        button.setObjectName('display_single_record_button_clicked')
+        button.clicked.connect(self.display_single_record_button_clicked)
+        button.setSizePolicy(
+            QtWidgets.QSizePolicy.Preferred,
+            QtWidgets.QSizePolicy.Preferred)
+        self.button_layout.addWidget(button)
+        # edit record button
+        button = QtWidgets.QToolButton()
+        button.setText(menu_option_titles[5])
+        button.setObjectName('edit_record_button_clicked')
+        button.clicked.connect(self.edit_record_button_clicked)
+        button.setSizePolicy(
+            QtWidgets.QSizePolicy.Preferred,
+            QtWidgets.QSizePolicy.Preferred)
+        self.button_layout.addWidget(button)
+        # delete record button
+        button = QtWidgets.QToolButton()
+        button.setText(menu_option_titles[6])
+        button.setObjectName('delete_record_button_clicked')
+        button.clicked.connect(self.delete_record_button_clicked)
+        button.setSizePolicy(
+            QtWidgets.QSizePolicy.Preferred,
+            QtWidgets.QSizePolicy.Preferred)
+        self.button_layout.addWidget(button)
+        # exit record button
+        button = QtWidgets.QToolButton()
+        button.setText(menu_option_titles[7])
+        button.setObjectName('exit_pychat_button_clicked')
+        button.clicked.connect(self.exit_pychat_button_clicked)
+        button.setSizePolicy(
+            QtWidgets.QSizePolicy.Preferred,
+            QtWidgets.QSizePolicy.Preferred)
+        self.button_layout.addWidget(button)
+
+        self.status_label = QtWidgets.QLabel('No button clicked')
+        self.output_textarea = QtWidgets.QTextEdit(
+            "Messages will appear here...")
+        self.output_textarea.setReadOnly(True)
+        self.output_textarea.setMinimumSize(QtCore.QSize(40, 80))
+        self.output_textarea.setLineWrapMode(QtWidgets.QTextEdit.NoWrap)
+
+        font = self.output_textarea.font()
+        font.setFamily("Courier")
+        font.setPointSize(10)
+        # add buttons layout and status lable to widget
+        self.widget_layout.addItem(self.button_layout)
+        self.widget_layout.addWidget(self.status_label)
+        self.widget_layout.addWidget(self.output_textarea)
+        # add widget to main layout
+        main_layout.addLayout(self.widget_layout)
+        self.statusBar()
+        self.statusBar().showMessage("Greg McLeod - mcle0463 - 2019")
+        self.setGeometry(500, 150, 350, 400)
+        self.setWindowTitle('Python Chat - Greg McLeod')
+        self.show()
+
+    def reload_button_clicked(self):
+        sender = self.sender()
+        self.status_label.setText("Currently selected: " + sender.text())
+        self.control.get_rows()
+        self.output_textarea.setText(self.control.print_data())
+
+    def display_full_data_button_clicked(self):
+        sender = self.sender()
+        self.status_label.setText("Currently selected: " + sender.text())
+        self.output_textarea.setText(self.control.print_data())
+        # self.control.print_data()
+
+    def create_new_record_button_clicked(self):
+        sender = self.sender()
+        self.status_label.setText("Currently selected: " + sender.text())
+        text, ok = QtWidgets.QInputDialog.getText(
+            self, 'New Record', 'Enter record data:')
+        if ok:
+            self.control.create_record(str(text))
+        self.output_textarea.setText(self.control.print_data())
+
+    def select_record_button_clicked(self):
+        dialog_text = "Enter a record to select between 1 and 10"
+        record_selection, ok = QtWidgets.QInputDialog.getInt(
+            self, 'Select Record', dialog_text)
+        if ok:
+            self.control.select_record(record_selection)
+        self.output_textarea.setText(self.control.print_data())
+
+    def display_single_record_button_clicked(self):
+        sender = self.sender()
+        self.status_label.setText("Currently selected: " + sender.text())
+        msg = QtWidgets.QMessageBox()
+        msg.setIcon(QtWidgets.QMessageBox.Information)
+        msg.setText(self.control.display_one_record())
+        msg.setStandardButtons(
+            QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
+        msg.setWindowTitle("Diplay record")
+        msg.exec_()
+
+    def edit_record_button_clicked(self):
+        sender = self.sender()
+        self.status_label.setText("Currently selected: " + sender.text())
+        text, ok = QtWidgets.QInputDialog.getText(
+            self, 'Text Input Dialog', 'Enter record data:')
+        if ok:
+            self.control.edit_record(str(text))
+        self.output_textarea.setText(self.control.print_data())
+
+    def delete_record_button_clicked(self):
+        sender = self.sender()
+        self.status_label.setText("Currently selected: " + sender.text())
+        self.control.delete_record()
+        self.output_textarea.setText(self.control.print_data())
+
+    def exit_pychat_button_clicked(self):
+        sender = self.sender()
+        self.status_label.setText("Currently selected: " + sender.text())
+        self.control.quit_pythonchat()
